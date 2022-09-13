57f1106201eed571f8860360b9194c1532626b04
Sergey Batukov <89929657+chepuhasasha@users.noreply.github.com>
2022-09-13 16:15:45 +0300

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { writeFile } from "node:fs/promises";
 
 try {
-  await fs.writeFile(__dirname + "test.md", "# 123");
+  await writeFile("test.md", "# 123");
 } catch (e) {
   console.log(e);
 }
